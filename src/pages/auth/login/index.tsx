35bb013ed8e29252850134e879b600e78988af60
hotanphong <phongho763@gmail.com>
2025-02-13 01:36:22 +0700

    add login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,9 +1,12 @@
-import LoginComponent from "@/components/auth/login-form";
+import {LoginComponent} from "@/components/auth/login-form";
 
-const LoginPage : React.FC = () => {
+
+const LoginPage: React.FC = () => {
     return (
-        <div>
-            <LoginComponent />
+        <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <LoginComponent/>
+            </div>
         </div>
     );
 }
