e3b12d04d2804ab89f4f1786480f8992cc7b8a63
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-02-14 18:50:29 +0700

    update login component

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,9 +1,11 @@
-import RegisterFormComponent from "@/components/auth/register-form";
+import {RegisterComponent} from "@/components/auth/register-form";
 
 const Register: React.FC = () => {
     return (
-        <div>
-            <RegisterFormComponent />
+        <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <RegisterComponent/>
+            </div>
         </div>
     );
 }
